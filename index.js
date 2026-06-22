3878e562fb01b4db5065a99e096bd1ab9e9e6cd1
praharshamaroju <marojupraharsha@gmail.com>
2026-06-22 12:37:23 +0530

    data types

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("hello")
+console.log("hello");
+let name="roh";
+let age=20;
+
+var a=20;
+a=30;
+console.log(a);
+//data types
+
+//number
+
+let b=2.56;
+//string
+let d="praharsha";
+console.log(d);
+//boolean
+let l=true;
+//undefined
+let u;
+//const p;
+console.log(u);
+//bigint
+let bg=23232852965656;
+console.log(bg);
+//null
+let app="null";
+console.log(app);
+//symbol
+
+//console.log(idn);
+let arr=[10,20,30,"ram",true];
+console.log(arr);
+//object
+let obj={
+  name:"ram",
+  id:12214,
+age:15,
+cat:"gen"
+}
+//function
+let s=function add(){
+  console.log("hello");
+}
+
+s();
