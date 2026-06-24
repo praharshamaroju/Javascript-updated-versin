c1a1763cd6099928364dd650f108a36762b5b2aa
praharshamaroju <marojupraharsha@gmail.com>
2026-06-24 12:55:08 +0530

    loops

---
+++ conditional.js
@@ -0,0 +1,26 @@
+//for
+for(let i =0;i<=10;i++){
+  console.log(i);
+}
+//while
+let k=0;
+while(k<10){
+  console.log(k)
+  k++;
+}
+//do while
+let j =0;
+do{
+  console.log(i);
+  i++;
+
+}while(j<10);
+
+//ifelse
+let p=8;
+if(p>=18){
+  console.log("elg");
+}
+else{
+  console.log("not");
+}
